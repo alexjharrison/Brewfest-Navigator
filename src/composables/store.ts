20bbe0c76245e4a0d1f0cf28b386245cf7a367c5
Alex Harrison <alexjharrison@gmail.com>
2023-02-09 21:30:19 -0500

    make buildable

--- src/composables/store.ts
+++ src/composables/store.ts
@@ -3,6 +3,17 @@ import beers from "../assets/beers";
 import breweries from "../assets/breweries";
 
 const isSidebarOpen = ref(false);
+console.log(
+  beers.map((beer) => {
+    /* @ts-ignore  */
+    const table = breweries[beer.breweryName];
+    if (!table) console.log(beer);
+    return {
+      ...beer,
+      table,
+    };
+  })
+);
 
 export function useStore() {
   return { beers, isSidebarOpen };
